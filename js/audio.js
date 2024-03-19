732831da0a0a934a841de57377cb489e0d5e8d99
SANKA CREW <158070400+sankalk@users.noreply.github.com>
2024-03-19 20:28:04 +0530

    Update audio.js

--- js/audio.js
+++ js/audio.js
@@ -1,11 +1,9 @@
-document.addEventListener("DOMContentLoaded", function () {
-    var audio = document.getElementById("autoplay loop");
+var audio = document.getElementById("autoplay loop");
 
-    // Check if there's a playback state stored in local storage
-    var playbackState = localStorage.getItem("audioPlaybackState");
+// Check if there's a playback state stored in local storage
+var playbackState = localStorage.getItem("audioPlaybackState");
 
-    // If there's a playback state, resume playback
-    if (playbackState === "playing") {
-        audio.play();
-    }
-});
+// If there's a playback state, resume playback
+if (playbackState === "playing") {
+    audio.play();
+}
